8ccf855ead60acb614044e4959697bea2fc0304a
Avi2124 <avikitaliya2018@gmail.com>
2026-06-19 14:24:39 +0530

    Upload on Github

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -37,49 +37,35 @@ export const getStudents = async (req, res, next) => {
       search = "",
       sort = "id"
     } = req.query;
-
     page = Number(page);
     limit = Number(limit);
-
     if (isNaN(page) || page < 1) page = 1;
     if (isNaN(limit) || limit < 1) limit = 5;
-
     const offset = (page - 1) * limit;
-
     let query = "SELECT * FROM students";
     let values = [];
     let conditions = [];
-
     if (city) {
       conditions.push("city = ?");
       values.push(city);
     }
-
     if (search) {
       conditions.push("name LIKE ?");
       values.push(`%${search}%`);
     }
-
     if (conditions.length > 0) {
       query += " WHERE " + conditions.join(" AND ");
     }
-
-    // SAFE SORT
     const allowedSort = ["id", "name", "age", "city"];
     if (!allowedSort.includes(sort)) {
       sort = "id";
     }
-
     query += ` ORDER BY ${sort} LIMIT ${Number(limit)} OFFSET ${Number(offset)}`;
-    // values.push(limit, offset);
-
     const [students] = await pool.execute(query, values);
-
     res.json({
       success: true,
-      data: students || []   // IMPORTANT SAFETY
+      data: students || [] 
     });
-
   } catch (error) {
     console.log("GET STUDENTS ERROR:", error);
     res.status(500).json({
