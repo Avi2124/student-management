9b5bb714e04c0cdefaff59c9f7fbb7be901a190a
Avi2124 <avikitaliya2018@gmail.com>
2026-06-19 09:49:07 +0530

    Upload on Github

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    port: 3306,
+    port: 4000,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
